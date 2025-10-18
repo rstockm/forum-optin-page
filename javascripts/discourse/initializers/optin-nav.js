fa228f778b38c899a5528f149a3509e58053a9ba
Ralf Stockmann <rstockm@gmail.com>
2025-10-18 02:21:11 +0200

    Use api.headerIcons.add instead of deprecated addToHeaderIcons

--- javascripts/discourse/initializers/optin-nav.js
+++ javascripts/discourse/initializers/optin-nav.js
@@ -1,17 +1,13 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
-import { h } from "virtual-dom";
 
 export default {
   name: "optin-nav",
   initialize() {
-    withPluginApi("0.8.40", (api) => {
-      api.addToHeaderIcons((helper) => {
-        return {
-          title: "Forum-Opt-in",
-          icon: "bell",
-          href: "/categories?show_optin=true",
-          className: "optin-link-icon",
-        };
+    withPluginApi("1.2.0", (api) => {
+      api.headerIcons.add("optin", {
+        title: "Forum-Opt-in",
+        icon: "bell",
+        href: "/categories?show_optin=true",
       });
     });
   },
