ca5e336731c3517d49641df78b8d430803347c9b
rstockm <rstockm@gmail.com>
2026-02-07 16:51:30 +0100

    Load category notification levels for opt-in view.
    Use store categories when opt-in is enabled so bell status reflects user settings.

--- javascripts/discourse/connectors/above-discovery-categories/optin-view.js
+++ javascripts/discourse/connectors/above-discovery-categories/optin-view.js
@@ -1,13 +1,27 @@
 import { getOwner } from "@ember/application";
 
 export default {
-  setupComponent(args, component) {
+  async setupComponent(args, component) {
     const params = new URLSearchParams(window.location.search);
     const showOptinView = params.get("show_optin") === "true";
 
     const owner = getOwner(component);
     const siteService = owner && owner.lookup("service:site");
-    const categories = (siteService && siteService.get("categories")) || [];
+    const store = owner && owner.lookup("service:store");
+
+    let categories = [];
+    if (showOptinView && store && store.findAll) {
+      try {
+        const storeCategories = await store.findAll("category");
+        categories = storeCategories.toArray
+          ? storeCategories.toArray()
+          : storeCategories;
+      } catch (e) {
+        categories = (siteService && siteService.get("categories")) || [];
+      }
+    } else {
+      categories = (siteService && siteService.get("categories")) || [];
+    }
 
     const parents = categories.filter((c) => !c.parent_category_id);
     const groupedCategories = parents.map((parent) => ({
